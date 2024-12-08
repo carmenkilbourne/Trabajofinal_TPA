efe552847f9b2ab587d6bf1eeba79996b78c56f4
carmenkilbourne <113438693+carmenkilbourne@users.noreply.github.com>
2024-12-09 00:27:32 +0100

    optimizando codigo

---
+++ Videojuego/src/interfaces/IMovimiento.java
@@ -0,0 +1,17 @@
+package interfaces;
+
+public interface IMovimiento {
+	/*
+	 * void moverArriba();
+	 * 
+	 * void moverAbajo();
+	 * 
+	 * void moverIzquierda();
+	 * 
+	 * void moverDerecha();
+	 * 
+	 * void atacar();
+	 * 
+	 * void defender();
+	 */
+}
