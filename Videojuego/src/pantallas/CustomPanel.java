6aadd5b8fc4ed41b8e1aef877d639bd905925503
carmenkilbourne <113438693+carmenkilbourne@users.noreply.github.com>
2024-12-08 23:50:12 +0100

    optimizando clase partida

---
+++ Videojuego/src/pantallas/CustomPanel.java
@@ -0,0 +1,23 @@
+package pantallas;
+
+import java.awt.Graphics;
+import javax.swing.JPanel;
+
+public class CustomPanel extends JPanel {
+    /**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private MouseInput mouseInput;
+
+    public CustomPanel(MouseInput mouseInput) {
+        this.mouseInput = mouseInput;
+        addMouseListener(mouseInput);
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        mouseInput.render(g);
+    }
+}
---
+++ Videojuego/src/pantallas/FondoPantalla.java
@@ -0,0 +1,26 @@
+package pantallas;
+
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.Toolkit;
+import javax.swing.JPanel;
+
+public class FondoPantalla extends JPanel {
+    /**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private Image imagen;
+
+    public FondoPantalla(String path) {
+        imagen = Toolkit.getDefaultToolkit().createImage(getClass().getResource(path));
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        if (imagen != null) {
+            g.drawImage(imagen, 0, 0, getWidth(), getHeight(), this);
+        }
+    }
+}
