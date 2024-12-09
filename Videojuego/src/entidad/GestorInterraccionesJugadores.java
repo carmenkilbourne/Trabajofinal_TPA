7e60b2813d51b8665df61367f7a35ac9f392c2f5
carmen.kilbourne <carmen.kilbourne@gmail.com>
2024-12-09 17:12:38 +0100

    o

---
+++ Videojuego/src/entidad/GestorInterraccionesJugadores.java
@@ -0,0 +1,63 @@
+package entidad;
+
+import java.awt.Image;
+
+import pantallas.Controlador;
+
+public class GestorInterraccionesJugadores {
+    private Jugador1 jugador1;
+    private Jugador2 jugador2;
+    MovimientoJugador movimientojugador = new MovimientoJugador();
+    MovimientoJugador2 movimientoJugador2 = new MovimientoJugador2();
+    private int areaEfectividad = 160;
+    private int areaEfectividady = 160;
+
+
+    public GestorInterraccionesJugadores(Jugador1 jugador1, Jugador2 jugador2,MovimientoJugador movimientojugador,MovimientoJugador2 movimientoJugador2 ) {
+        this.jugador1 = jugador1;
+        this.jugador2 = jugador2;
+        this.movimientojugador = movimientojugador;
+        this.movimientoJugador2 = movimientoJugador2;
+    }
+
+    public void actualizarMovimiento() {
+		if (esEfectivo()) {
+			if (movimientojugador.atacar) {
+				jugador1.atacar(jugador2);
+				movimientojugador.atacar = false; // reiniciar el ataque para evitar múltiples ataques continuos
+			}
+			// Condición para que el jugador 2 ataque al jugador 1
+			if (movimientoJugador2.atacar) {
+				jugador2.atacar(jugador1);
+				movimientoJugador2.atacar = false;
+			}
+			if (movimientojugador.patada) {
+				jugador1.atacar(jugador2);
+				movimientojugador.patada = false; // reiniciar el ataque para evitar múltiples ataques continuos
+			}
+			if (movimientoJugador2.patada) {
+				jugador1.atacar(jugador1);
+				movimientoJugador2.patada = false; // reiniciar el ataque para evitar múltiples ataques continuos
+			}
+		}
+		jugador1.setDefendiendo(movimientojugador.defensa);
+		jugador2.setDefendiendo(movimientoJugador2.defensa);
+		partidaAcabada();
+    }
+
+    public boolean esEfectivo() {
+    	areaEfectividad = Math.abs(jugador1.getX()-jugador2.getX());
+    	areaEfectividady = Math.abs(jugador1.getY()-jugador2.getY());
+		 if (areaEfectividad <= 160 && areaEfectividady == 0) {
+			 return true;
+		 }
+		 if(jugador1.)
+		 //logica que me falta es que solo es efectivo si ambos se estan mirando
+		 return false;
+	 }
+
+    public boolean partidaAcabada() {	//metodo que devuelve true si la partida ha acabado
+        return jugador1.getSaludActual() == 0 || jugador2.getSaludActual() == 0;
+    }
+}
+
